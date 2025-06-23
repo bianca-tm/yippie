f306d75d80e0ac2e632f0b0ddc65b3c4715647ac
bianca-tm <tech.btovalin@gmail.com>
2025-06-23 18:41:50 -0400

    fix header and styling

--- app/header.tsx
+++ app/header.tsx
@@ -1,80 +1,76 @@
 'use client'
 import { TextEffect } from '@/components/ui/text-effect'
 import Link from 'next/link'
-import React, { useState } from 'react';
-import { HiOutlineXMark, HiBars3 } from 'react-icons/hi2';
+import React from 'react';
 import { ThemeSwitch } from '@/components/ui/theme-switch';
 import { menuItems } from "@/app/data/data";
 
 const Header: React.FC = () => {
-    const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
-  return (
-    <header className="mb-8 flex items-center justify-between">
-        {/* TODO: fix styling*/}
-        <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-10 w-full">
-      <div className="flex flex-col justify-center">
-        <Link href="/" className="font-medium text-black dark:text-white">
-          Bianca Tovalin-Mangual
-        </Link>
-        <TextEffect
-          as="p"
-          preset="fade"
-          per="char"
-          className="text-zinc-600 dark:text-zinc-500"
-          delay={0.25}
-        >
-          Tech enjoyer
-        </TextEffect>
-      </div>
-      {/* Desktop Menu */}
-    <div className="flex items-center">
-      <ul className="hidden md:flex space-x-6">
-        {menuItems.map(item => (
-            <li key={item.text}>
-              <Link href={item.url} className="text-foreground hover:text-foreground-accent transition-colors">
-                {item.text}
-              </Link>
-            </li>
-        ))}
-          <li>
-              <a
-                  href="/tovalin-mangual_bianca_resume.pdf"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-foreground hover:text-foreground-accent transition-colors"
-              >
-                  Resume
-              </a>
-          </li>
-          <ThemeSwitch />
-      </ul>
-            {/* Mobile Menu Button */}
-            <div className="md:hidden flex items-center">
-                <button
-                    onClick={toggleMenu}
-                    type="button"
-                    className="bg-primary text-black focus:outline-none rounded-full w-10 h-10 flex items-center justify-center"
-                    aria-controls="mobile-menu"
-                    aria-expanded={isOpen}
-                >
-                    {isOpen ? (
-                        <HiOutlineXMark className="h-6 w-6" aria-hidden="true" />
-                    ) : (
-                        <HiBars3 className="h-6 w-6" aria-hidden="true" />
-                    )}
-                    <span className="sr-only">Toggle navigation</span>
-                </button>
+    return (
+        // TODO: make top text and menu items aligned with the other sections
+        <header className="w-full bg-white dark:bg-black transition-colors duration-300">
+            <div className="max-w-3xl mx-auto px-5 mb-8">
+                <nav className="flex justify-between items-center py-2 md:py-10 w-full">
+                    <div className="flex flex-col md:flex-row md:items-center md:justify-between w-full">
+                        <div className="flex flex-col">
+                            <Link href="/" className="font-medium text-black dark:text-white">
+                                Bianca Tovalin-Mangual
+                            </Link>
+                            <TextEffect
+                                as="p"
+                                preset="fade"
+                                per="char"
+                                className="text-zinc-600 dark:text-zinc-500"
+                                delay={0.25}
+                            >
+                                Tech enjoyer
+                            </TextEffect>
+                        </div>
+                        <ul className="flex flex-row flex-wrap items-center space-x-6 mt-4 md:mt-0">
+                            {menuItems.map(item => (
+                                <li key={item.text}>
+                                    <Link
+                                        href={item.url}
+                                        className="text-foreground hover:text-foreground-accent transition-colors text-sm"
+                                    >
+                                        <TextEffect
+                                            as="span"
+                                            preset="fade"
+                                            per="char"
+                                            className="inline text-zinc-600 dark:text-zinc-500"
+                                            delay={0.5}
+                                        >
+                                            {item.text}
+                                        </TextEffect>
+                                    </Link>
+                                </li>
+                            ))}
+                            <li>
+                                <a
+                                    href={"/tovalin-mangual_bianca_resume.pdf"}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-foreground hover:text-foreground-accent transition-colors text-sm"
+                                >
+                                    <TextEffect
+                                        as="span"
+                                        preset="fade"
+                                        per="char"
+                                        className="inline text-zinc-600 dark:text-zinc-500"
+                                        delay={0.5}
+                                    >
+                                        Resume
+                                    </TextEffect>
+                                </a>
+                            </li>
+                            <ThemeSwitch />
+                        </ul>
+                    </div>
+                </nav>
             </div>
-        </div>
-        </nav>
-
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Header;
