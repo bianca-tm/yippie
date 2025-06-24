2f76e64730cce1ecba70b56f3c77e3a9d56b1dea
bianca-tm <tech.btovalin@gmail.com>
2025-06-24 17:03:25 -0400

    adjust project structure

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,38 +1,90 @@
 type Project = {
     name: string
     description: string
-    link: string
-    video: string
+    link?: string
+    video?: string
     id: string
     category: 'swe' | 'data' | 'creative'
 }
 
 export const PROJECTS: Project[] = [
     {
-        name: 'Motion Primitives Pro',
+        name: 'swe placeholder',
         description:
-            'Advanced components and templates to craft beautiful websites.',
-        link: 'https://pro.motion-primitives.com/',
-        video:
-            'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+            'description of swe placeholder 1',
         id: 'project1',
         category: 'swe',
     },
     {
-        name: 'Motion Primitives',
-        description: 'UI kit to make beautiful, animated interfaces.',
-        link: 'https://motion-primitives.com/',
-        video:
-            'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+        name: 'swe placeholder 2',
+        description:
+            'description of swe placeholder 2',
         id: 'project2',
+        category: 'swe',
+    },
+    {
+        name: 'swe placeholder 3',
+        description:
+            'description of swe placeholder 3',
+        // link: 'https://pro.motion-primitives.com/',
+        // video:
+        //     'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+        id: 'project3',
+        category: 'swe',
+    },
+    {
+        name: 'swe placeholder 4',
+        description:
+            'description of swe placeholder 4',
+        id: 'project4',
+        category: 'swe',
+    },
+    {
+        name: 'data placeholder',
+        description: 'description of data placeholder 1',
+        id: 'project5',
         category: 'data',
     },
     {
-        name: 'jucie',
-        description: 'random fruits!',
-        link: '',
-        video: '',
-        id: 'project3',
+        name: 'data placeholder 2',
+        description: 'description of data placeholder 2',
+        id: 'project6',
+        category: 'data',
+    },
+    {
+        name: 'data placeholder 3',
+        description: 'description of data placeholder 3',
+        id: 'project7',
+        category: 'data',
+    },
+    {
+        name: 'data placeholder 4',
+        description: 'description of data placeholder 4',
+        id: 'project8',
+        category: 'data',
+    },
+    {
+        name: 'creative placeholder',
+        description: 'description of creative placeholder 1',
+        id: 'project9',
+        category: 'creative',
+    },
+    {
+        name: 'creative placeholder 2',
+        description: 'description of creative placeholder 2',
+        id: 'project10',
+        category: 'creative',
+    },
+    {
+        name: 'creative placeholder 3',
+        description: 'description of creative placeholder 3',
+        id: 'project11',
+        category: 'creative',
+    },
+    {
+        name: 'creative placeholder 4',
+        description: 'description of creative placeholder 4',
+        id: 'project12',
         category: 'creative',
     },
 ]
